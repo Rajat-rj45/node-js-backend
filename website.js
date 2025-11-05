b2b49f1ae1ddfa5dac34ac19296327c9370a28de
Rajat-rj45 <rajatsuryavanshi404@gmail.com>
2025-11-05 16:47:33 +0530

    hi

---
+++ website.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require("fs");
+
+http.createServer((req, res)=>{
+    fs.readFile("html/index.html",'utf-8',(err, data)=>{
+        if(err){
+            res.writeHead(500,{"content-type":"text/plain"})
+            res.end("internal server error");
+            return false
+        }
+        res.write(data);
+        res.end();
+
+    })
+
+
+}).listen(3200);
