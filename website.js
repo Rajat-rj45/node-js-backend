23e3a72ff10341c336b48b4e7181bcb94e3bcc84
Rajat-rj45 <rajatsuryavanshi404@gmail.com>
2025-11-06 13:53:06 +0530

    cdvc

--- website.js
+++ website.js
@@ -1,17 +1,74 @@
-const http = require('http');
+const http = require("http");
 const fs = require("fs");
 
-http.createServer((req, res)=>{
-    fs.readFile("html/index.html",'utf-8',(err, data)=>{
-        if(err){
-            res.writeHead(500,{"content-type":"text/plain"})
-            res.end("internal server error");
-            return false
+http
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      fs.readFile("html/index.html", "utf-8", (err, data) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Internal Server Error");
+          return;
         }
-        res.write(data);
-        res.end();
-
-    })
-
-
-}).listen(3200);
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+    } else if (req.url === "/about") {
+      fs.readFile("html/about.html", "utf-8", (err, data) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Internal Server Error");
+          return;
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+    } else if (req.url === "/services") {
+      fs.readFile("html/services.html", "utf-8", (err, data) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Internal Server Error");
+          return;
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+    } else if (req.url === "/gallery") {
+      fs.readFile("html/gallery.html", "utf-8", (err, data) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Internal Server Error");
+          return;
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+    } else if (req.url === "/contact") {
+      fs.readFile("html/contact.html", "utf-8", (err, data) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Internal Server Error");
+          return;
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+    }else if (req.url === "/form") {
+      fs.readFile("html/form.html", "utf-8", (err, data) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Internal Server Error");
+          return;
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+    }     else {
+      // 404 page
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("404 Page Not Found");
+    }
+  })
+  .listen(3200, () => {
+    console.log("Server running on http://localhost:3200");
+  });
